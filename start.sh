d2fa457e7bf5566fef23cd445c6a509293f082e0
Anderson Andrade Silva <ander.math89@gmail.com>
2025-02-16 16:44:25 -0300

    Correção: "Corrigindo erro de porta no config.toml e start.sh"

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Se $PORT não estiver definido, usa 8501 como padrão (localmente)
+PORT=${PORT:-8501}
+
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0 --server.headless true
+
 
